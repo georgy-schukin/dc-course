b490fb22dacf2411fcb77cbfdfb6306133a8ca99
Georgy Schukin <georgy.schukin@gmail.com>
2022-10-16 19:13:04 +0600

    Add web server example

---
+++ examples/WebServer/src/main/java/org/example/Processor.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Processor of HTTP request.
+ */
+public class Processor {
+    private final Socket socket;
+    private final HttpRequest request;
+
+    public Processor(Socket socket, HttpRequest request) {
+        this.socket = socket;
+        this.request = request;
+    }
+
+    public void process() throws IOException {
+        System.out.println("Got request:");
+        System.out.println(request.toString());
+        System.out.flush();
+
+        PrintWriter output = new PrintWriter(socket.getOutputStream());
+
+        // We are returning a simple web page now.
+        output.println("HTTP/1.1 200 OK");
+        output.println("Content-Type: text/html; charset=utf-8");
+        output.println();
+        output.println("<html>");
+        output.println("<head><title>Hello</title></head>");
+        output.println("<body><p>Hello, world!</p></body>");
+        output.println("</html>");
+        output.flush();
+        socket.close();
+    }
+}
---
+++ examples/WebServer/src/main/java/org/example/WebServer.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Simple web server.
+ */
+public class WebServer {
+    public static void main(String[] args) {
+        // Port number for http request
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
+        // The maximum queue length for incoming connection
+        int queueLength = args.length > 2 ? Integer.parseInt(args[2]) : 50;;
+
+        try (ServerSocket serverSocket = new ServerSocket(port, queueLength)) {
+            System.out.println("Web Server is starting up, listening at port " + port + ".");
+            System.out.println("You can access http://localhost:" + port + " now.");
+
+            while (true) {
+                // Make the server socket wait for the next client request
+                Socket socket = serverSocket.accept();
+                System.out.println("Got connection!");
+
+                // To read input from the client
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+
+                // Get request
+                HttpRequest request = HttpRequest.parse(input);
+
+                // Process request
+                Processor proc = new Processor(socket, request);
+                proc.process();
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            System.out.println("Server has been shutdown!");
+        }
+    }
+}
